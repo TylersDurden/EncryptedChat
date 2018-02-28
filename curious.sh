f9a14b3cd2b3f663b8431323d845ad01b0548658
Scott <31575629+TylersDurden@users.noreply.github.com>
2018-02-27 21:35:40 -0500

    Update curious.sh

--- curious.sh
+++ curious.sh
@@ -1,11 +1,5 @@
-#cd /projects/Data/src
+#!/bin/bash
 # Compile java program 
 javac Messenger.java
-# create dumplogs for hashing 
-#touch user1m1s1.txt 
-#touch m0digest.txt
-# Run program, sending message from example.txt 
-#out = "/projects/Data/EncryptedChat-v0.1/src./example.txt"
-#echo 'Enter message and press [ENTER]:'
-#read message > example.txt
+# send example.txt file through encrypted chat to whoever is at other end
 java Messenger -S /projects/Data/EncryptedChat-v0.1/src example.txt 
