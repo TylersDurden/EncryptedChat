4a97094daeb2c8b470e7935d4ae3c6bc83353e78
Scott <31575629+TylersDurden@users.noreply.github.com>
2018-02-27 21:40:23 -0500

    New encrypted communication mechanism

--- recv.sh
+++ recv.sh
@@ -1,5 +1,10 @@
+#!/bin/bash
+#Compile Programs for receiving a message
 javac Messenger.java
 javac Linguist.java 
 # Receieve messages to output.txt
 touch output.txt
-java Messenger -R /projects/Data/EncryptedChat-v0.1/src output.txt
+java Messenger -R /projects/Data/EncryptedChat-v0.1/src > output.txt
+# Validate file 
+touch verify.txt
+cksum output.txt > verify.txt
